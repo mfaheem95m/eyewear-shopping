8dc7b6071b4a53e8fbc68c039135f50dfc532ffb
Muhammad Faheem <mfaheem95m@gmail.com>
2022-01-26 21:07:06 +0500

    create loginhoc component

---
+++ src/loginHoc.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import myContext from './Context'
+const loginHoc = (WrapperComponent) => {
+  return class Hoc extends React.Component{
+    constructor(props){
+      super(props)
+    }
+
+    static contextType = myContext
+    render(){
+      const {isLogin} = this.context
+
+      return(
+        <WrapperComponent isLogin={isLogin}  {...this.props} />
+      )
+    }
+  }
+}
+
+
+export default loginHoc;
